9e95f4b57e8f83329aed70b867f6fc029d72287d
Vishnu Rajendran <rajendran.vishnu97@gmail.com>
2022-12-17 12:20:53 +0530

    Updates to layout

---
+++ Client/Aiyo-client/Aiyo-client/Controls/MessageBubble.xaml.cs
@@ -0,0 +1,9 @@
+namespace Aiyo_Client.Controls;
+
+public partial class MessageBubble : ContentView
+{
+	public MessageBubble()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Client/Aiyo-client/Aiyo-client/HomePage.xaml.cs
@@ -0,0 +1,12 @@
+using Aiyo_Client.Controls;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Aiyo_Client;
+
+public partial class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		InitializeComponent();
+	}
+}
